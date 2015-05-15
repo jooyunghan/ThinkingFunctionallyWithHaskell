6feaccdaec7d03fe0c45e3b76cf42363390901f5
Jooyung Han <jooyung.han@gmail.com>
2015-05-14 21:34:47 -0700

    Add types and re-implement countRuns without group

--- src/Words.hs
+++ src/Words.hs
@@ -1,13 +1,23 @@
 module Words where
 
-import Data.Char
-import Data.List
+import Data.Char (toLower)
+import Data.List (sort)
 
-countRuns = map (\g -> (length g, head g)) . group
+type Word = [Char]
 
+type Run = (Int, Word)
+
+countRuns :: [Word] -> [Run]
+countRuns [] = []
+countRuns (x:xs) =  let count = 1 + length (takeWhile (== x) xs)
+                        in (count, x) : countRuns (dropWhile (== x) xs)
+
+showRun :: Run -> String
 showRun (count, word) = word ++ ": " ++ (show count) ++ "\n"
 
+sortRuns :: [Run] -> [Run]
 sortRuns = reverse . sort
 
+commonWords :: Int -> String -> String
 commonWords n = concat . map showRun . take n . sortRuns .countRuns . sort . words . map toLower
 
